f32f0b11f024f471432e23f5b07cb7657f9e1ffd
Facundo Arambillet <Facundoarambillet@gmail.com>
2024-02-28 20:05:23 -0300

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GreetingComponent } from './greeting/greeting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: GreetingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpBackend, HttpClientModule } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { ComplaintsClientModule } from 'complaints-client';
+import { MultiTranslateHttpLoader } from 'ngx-translate-multi-http-loader';
+import { GreetingComponent } from './greeting/greeting.component';
+
+export function HttpLoaderFactory(httpBackend: HttpBackend) {
+  return new MultiTranslateHttpLoader(httpBackend, [
+    { prefix: '/assets/i18n/', suffix: '.json' },
+    { prefix: '/assets/complaints-client/i18n/', suffix: '.json' },
+    { prefix: '/assets/complaints-client/i18n/navlinks/', suffix: '.json' },
+  ])
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GreetingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ComplaintsClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpBackend]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
